16caa2a5be569c805e4c3df977811ed3b0d95dcf
jslay <justin.slay@gmail.com>
2018-07-23 00:59:58 -0600

    Added argparse and other optimizations

--- README.md
+++ README.md
@@ -14,6 +14,21 @@ pipenv install
 python3 script.py
 ```
 
+### Usage
+
+You can either run the script directly for interactive mode, or use the command line arguments.
+
+    python script.py
+    python script.py -u rfscVS0vtbw -o videos/ -f my_downloaded_video
+    python script.py -u rfscVS0vtbw -o audio/ -f my_downloaded_audio --audio-only
+    
+    -u --url            YouTube URL or YouTube Video ID to download
+    -o --output-path    Output Directory Path
+    -f --filename       Override the output filename. Does not override file extension
+    -a --audio-only     Download Auido Only
+    
+
+
 If you would like to suggest changes feel free to fork this repo and create PR or submit an issue
 
 Feel Free to DM on twitter if you have any questions
--- script.py
+++ script.py
@@ -1,61 +1,113 @@
+#!/usr/bin/env python3.6
 # A simple Python Script that will allow you download video
+import argparse
 
-import sys ,os
 from pytube import YouTube
-x = """\
-  $$\     $$\                $$$$$$$$\        $$\                        $$$$$$\                      $$\            $$\     
-  \$$\   $$  |               \__$$  __|       $$ |                      $$  __$$\                     \__|           $$ |    
-   \$$\ $$  /$$$$$$\  $$\   $$\ $$ |$$\   $$\ $$$$$$$\   $$$$$$\        $$ /  \__| $$$$$$$\  $$$$$$\  $$\  $$$$$$\ $$$$$$\   
-    \$$$$  /$$  __$$\ $$ |  $$ |$$ |$$ |  $$ |$$  __$$\ $$  __$$\       \$$$$$$\  $$  _____|$$  __$$\ $$ |$$  __$$\\_$$  _|  
-     \$$  / $$ /  $$ |$$ |  $$ |$$ |$$ |  $$ |$$ |  $$ |$$$$$$$$ |       \____$$\ $$ /      $$ |  \__|$$ |$$ /  $$ | $$ |    
-      $$ |  $$ |  $$ |$$ |  $$ |$$ |$$ |  $$ |$$ |  $$ |$$   ____|      $$\   $$ |$$ |      $$ |      $$ |$$ |  $$ | $$ |$$\ 
-      $$ |  \$$$$$$  |\$$$$$$  |$$ |\$$$$$$  |$$$$$$$  |\$$$$$$$\       \$$$$$$  |\$$$$$$$\ $$ |      $$ |$$$$$$$  | \$$$$  |
-      \__|   \______/  \______/ \__| \______/ \_______/  \_______|       \______/  \_______|\__|      \__|$$  ____/   \____/ 
-                                                                                                          $$ |               
-                                                                                                          $$ |               
-                                                                                                          \__|               
-"""
-print(x)
-while True:
-	user = input('To Download Video/Audio Y/n: ')
-	if user.lower() in ['yes', 'y']:
-		url = input('Enter Url: ')
-		link = YouTube(url)
-		form = input('Do you want to download video or audio: ')
-		if form.lower() in ['video', 'v']:
-			download_video = link.streams.first()
-			print('Download Started')
-			download_video.download()
-		elif form.lower() in ['audio', 'a']:
-			warn = input('Audio Downloads take longer Do you want to Continue Y/n: ')
-			if warn.lower() in ['yes', 'y']:
-				download_mp3 = link.streams.filter(only_audio=True).first()
-				print('Download Started')
-				download_mp3.download('audio/')
-			elif warn.lower() in ['no', 'n']:
-				vid = input('To Download Vid Y/n ')
-				if vid.lower() in ['yes', 'y']:
-					download_video = link.streams().first()
-					print('Download Started')
-					download_video.download('videos/')
-				elif vid.lower() in ['no', 'n']:
-					exit()
-	if user.lower() in ['no', 'n']:
-		print("""
-
-							$$$$$$$$\ $$\                           $$\                       $$$$$$$$\                        $$\   $$\           $$\                     
-\__$$  __|$$ |                          $$ |                      $$  _____|                       $$ |  $$ |          \__|                    
-   $$ |   $$$$$$$\   $$$$$$\  $$$$$$$\  $$ |  $$\  $$$$$$$\       $$ |    $$$$$$\   $$$$$$\        $$ |  $$ | $$$$$$$\ $$\ $$$$$$$\   $$$$$$\  
-   $$ |   $$  __$$\  \____$$\ $$  __$$\ $$ | $$  |$$  _____|      $$$$$\ $$  __$$\ $$  __$$\       $$ |  $$ |$$  _____|$$ |$$  __$$\ $$  __$$\ 
-   $$ |   $$ |  $$ | $$$$$$$ |$$ |  $$ |$$$$$$  / \$$$$$$\        $$  __|$$ /  $$ |$$ |  \__|      $$ |  $$ |\$$$$$$\  $$ |$$ |  $$ |$$ /  $$ |
-   $$ |   $$ |  $$ |$$  __$$ |$$ |  $$ |$$  _$$<   \____$$\       $$ |   $$ |  $$ |$$ |            $$ |  $$ | \____$$\ $$ |$$ |  $$ |$$ |  $$ |
-   $$ |   $$ |  $$ |\$$$$$$$ |$$ |  $$ |$$ | \$$\ $$$$$$$  |      $$ |   \$$$$$$  |$$ |            \$$$$$$  |$$$$$$$  |$$ |$$ |  $$ |\$$$$$$$ |
-   \__|   \__|  \__| \_______|\__|  \__|\__|  \__|\_______/       \__|    \______/ \__|             \______/ \_______/ \__|\__|  \__| \____$$ |
-                                                                                                                                     $$\   $$ |
-                                                                                                                                     \$$$$$$  |
-                                                                                                                                      \______/ 
 
 
+def get_header():
+    return """\
+      $$\     $$\                $$$$$$$$\        $$\                        $$$$$$\                      $$\            $$\     
+      \$$\   $$  |               \__$$  __|       $$ |                      $$  __$$\                     \__|           $$ |    
+       \$$\ $$  /$$$$$$\  $$\   $$\ $$ |$$\   $$\ $$$$$$$\   $$$$$$\        $$ /  \__| $$$$$$$\  $$$$$$\  $$\  $$$$$$\ $$$$$$\   
+        \$$$$  /$$  __$$\ $$ |  $$ |$$ |$$ |  $$ |$$  __$$\ $$  __$$\       \$$$$$$\  $$  _____|$$  __$$\ $$ |$$  __$$\\_$$  _|  
+         \$$  / $$ /  $$ |$$ |  $$ |$$ |$$ |  $$ |$$ |  $$ |$$$$$$$$ |       \____$$\ $$ /      $$ |  \__|$$ |$$ /  $$ | $$ |    
+          $$ |  $$ |  $$ |$$ |  $$ |$$ |$$ |  $$ |$$ |  $$ |$$   ____|      $$\   $$ |$$ |      $$ |      $$ |$$ |  $$ | $$ |$$\ 
+          $$ |  \$$$$$$  |\$$$$$$  |$$ |\$$$$$$  |$$$$$$$  |\$$$$$$$\       \$$$$$$  |\$$$$$$$\ $$ |      $$ |$$$$$$$  | \$$$$  |
+          \__|   \______/  \______/ \__| \______/ \_______/  \_______|       \______/  \_______|\__|      \__|$$  ____/   \____/ 
+                                                                                                              $$ |               
+                                                                                                              $$ |               
+                                                                                                              \__|               
+    """
 
-						""")
-		exit()
+
+def get_footer():
+    return """\n
+    $$$$$$$$\ $$\                           $$\                       $$$$$$$$\                        $$\   $$\           $$\                     
+    \__$$  __|$$ |                          $$ |                      $$  _____|                       $$ |  $$ |          \__|                    
+       $$ |   $$$$$$$\   $$$$$$\  $$$$$$$\  $$ |  $$\  $$$$$$$\       $$ |    $$$$$$\   $$$$$$\        $$ |  $$ | $$$$$$$\ $$\ $$$$$$$\   $$$$$$\  
+       $$ |   $$  __$$\  \____$$\ $$  __$$\ $$ | $$  |$$  _____|      $$$$$\ $$  __$$\ $$  __$$\       $$ |  $$ |$$  _____|$$ |$$  __$$\ $$  __$$\ 
+       $$ |   $$ |  $$ | $$$$$$$ |$$ |  $$ |$$$$$$  / \$$$$$$\        $$  __|$$ /  $$ |$$ |  \__|      $$ |  $$ |\$$$$$$\  $$ |$$ |  $$ |$$ /  $$ |
+       $$ |   $$ |  $$ |$$  __$$ |$$ |  $$ |$$  _$$<   \____$$\       $$ |   $$ |  $$ |$$ |            $$ |  $$ | \____$$\ $$ |$$ |  $$ |$$ |  $$ |
+       $$ |   $$ |  $$ |\$$$$$$$ |$$ |  $$ |$$ | \$$\ $$$$$$$  |      $$ |   \$$$$$$  |$$ |            \$$$$$$  |$$$$$$$  |$$ |$$ |  $$ |\$$$$$$$ |
+       \__|   \__|  \__| \_______|\__|  \__|\__|  \__|\_______/       \__|    \______/ \__|             \______/ \_______/ \__|\__|  \__| \____$$ |
+                                                                                                                                         $$\   $$ |
+                                                                                                                                         \$$$$$$  |
+                                                                                                                                          \______/ 
+    \n\n\n
+    """
+
+
+def interactive_mode():
+    print(get_header())
+    while True:
+        user = input('To Download Video/Audio Y/n: ')
+        if user.lower() in ['yes', 'y']:
+            url = input('Enter Url: ')
+            form = input('Do you want to download video or audio: ')
+            if form.lower() in ['video', 'v']:
+                download_youtube_video(url, output_path='videos/')
+
+            elif form.lower() in ['audio', 'a']:
+                warn = input('Audio Downloads take longer Do you want to Continue Y/n: ')
+                if warn.lower() in ['yes', 'y']:
+                    download_youtube_video(url, audio_only=True, output_path='audio/')
+
+                elif warn.lower() in ['no', 'n']:
+                    vid = input('To Download Vid Y/n ')
+                    if vid.lower() in ['yes', 'y']:
+                        download_youtube_video(url, output_path='videos/')
+
+                    elif vid.lower() in ['no', 'n']:
+                        exit()
+        if user.lower() in ['no', 'n']:
+            print(get_footer())
+            exit()
+
+
+def download_youtube_video(url, audio_only=False, output_path=None, filename=None, filename_prefix=None):
+    """
+    Download a YouTube Video.
+    :param url: Full URL to YouTube Video or YouTube Video ID
+    :type url: str
+    :param audio_only: Download only the audio for the video. Takes longer than video.
+    :type audio_only: bool
+    :param output_path: Path to folder to output file.
+    :type output_path: str
+    :param filename: Filename override. Does not override extension.
+    :type filename: str
+    :param filename_prefix: Currently Does Not Work on pytube
+    :type filename_prefix: str
+    :return: None
+    """
+    if 'https' not in url:
+        url = 'https://www.youtube.com/watch?v=%s' % url
+    video = YouTube(url)
+    if audio_only:
+        stream = video.streams.filter(only_audio=True).first()
+    else:
+        stream = video.streams.first()
+    print('Download Started: %s' % video.title)
+    stream.download(output_path=output_path, filename=filename)
+    print('Download Complete: %s' % video.title)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='YouTube Video/Audio Downloader')
+    parser.add_argument('-u', '--url', help='YouTube URL or YouTube Video ID to download', default=None)
+    parser.add_argument('-o', '--output-path', help='Output Directory Path', default=None)
+    parser.add_argument('-f', '--filename', help='Override the output filename. Does not override file extension',
+                        default=None)
+    # parser.add_argument('-p', '--filename_prefix', help='Filename Prefix', default=None) Currently does not work
+    parser.add_argument('-a', '--audio-only', help='Download Audio Only', action='store_true', default=False)
+
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    if args.url:
+        download_youtube_video(args.url, audio_only=args.audio_only,
+                               output_path=args.output_path, filename=args.filename)
+    else:
+        interactive_mode()
